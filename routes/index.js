e024f99fe2d56150e8f6890b9aeeb0a25990e893
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-07-09 11:43:13 -0300

    Estructura armada con html's

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/footer', function(req, res, next){
+  res.sendFile(path.join(__dirname,'.././views/footer.html'))
+})
+
+router.get('/login', function(req, res, next){
+  res.sendFile(path.join(__dirname,'.././views/login.html'))
+})
+
+router.get('/register', function(req, res, next){
+  res.sendFile(path.join(__dirname,'.././views/register.html'))
+})
+
+router.get('/productDetail', function(req, res, next){
+  res.sendFile(path.join(__dirname,'.././views/productDetail.html'))
+})
+
+router.get('/productCart', function(req, res, next){
+  res.sendFile(path.join(__dirname,'.././views/productCart.html'))
+})
+
 
 module.exports = router;
