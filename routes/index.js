b0968a63f2f8c953dc5983a5740a3ed211e6ccc4
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-07-13 18:38:33 -0300

    Add generalProduct

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,15 @@ var router = express.Router();
 var path = require('path')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.get('/footer', function(req, res, next){
+
+/* router.get('/footer', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/footer.html'))
-})
+}) */
 
-router.get('/login', function(req, res, next){
+/* router.get('/login', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/login.html'))
-})
+}) */
 
 router.get('/register', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/register.html'))
@@ -26,8 +24,11 @@ router.get('/productDetail', function(req, res, next){
 router.get('/productCart', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/productCart.html'))
 })
-router.get('/index', function(req, res, next){
+router.get('/', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/index.html'))
+})
+router.get('/product', function(req, res, next){
+  res.sendFile(path.join(__dirname,'.././views/generalProduct.html'))
 })
 
 
