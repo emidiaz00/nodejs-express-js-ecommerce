2a9056d48d21c5f9d79d870a0e41644024f4ceb9
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-07-16 09:35:51 -0300

    Routes fixed

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var path = require('path')
 /* GET home page. */
 
 
-router.get('/footer', function(req, res, next){
+/* router.get('/footer', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/footer.html'))
-}) 
+})  */
 
 /* router.get('/login', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/login.html'))
@@ -21,13 +21,13 @@ router.get('/productDetail', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/productDetail.html'))
 })
 
-router.get('/productCart', function(req, res, next){
+/* router.get('/productCart', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/productCart.html'))
-})
+}) */
 router.get('/', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/index.html'))
 })
-router.get('/product', function(req, res, next){
+router.get('/products', function(req, res, next){
   res.sendFile(path.join(__dirname,'.././views/generalProduct.html'))
 })
 
